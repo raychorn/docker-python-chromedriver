937e75cde96bee0765c233bc6686007b59992e14
Ray C Horn <raychorn@gmail.com>
2023-01-06 15:47:37 +0000

    latest.

--- py-debian/3.10-selenium/dobuild.sh
+++ py-debian/3.10-selenium/dobuild.sh
@@ -4,7 +4,7 @@
 docker-compose build --no-cache --force-rm --pull
 
 docker tag 310-selenium_python-chromedriver:latest 10.0.0.239:5000/310-selenium_python-chromedriver:latest
-docker tag 310-selenium_python-chromedriver:latest 10.0.0.120:49153/310-selenium_python-chromedriver:latest
+docker tag 310-selenium_python-chromedriver:latest devplayground.web-service.org:5000/310-selenium_python-chromedriver:latest
 
 docker push 10.0.0.239:5000/310-selenium_python-chromedriver:latest
-docker push 10.0.0.120:49153/310-selenium_python-chromedriver:latest
+docker push devplayground.web-service.org:5000/310-selenium_python-chromedriver:latest
